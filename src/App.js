7e30f793cd8a147cc702c5d3dcc1297aa785efd6
Wira Pratama <wirapratama758@gmail.com>
2020-11-27 16:06:00 +0800

    finish with small ecommerce page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Hero from './components/Hero';
+import Footer from './components/Footer';
+import Feature from './components/Feature';
+import Products from './components/Products';
+import { productData, productDataTwo } from './components/Products/data';
+import { GlobalStyle } from './globalStyle';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <GlobalStyle />
+      <Hero />
+      <Products heading="Choose your Pizza" data={productData}/>
+      <Feature />
+      <Products heading="Choose your Deserts" data={productDataTwo}/>
+      <Footer />
+    </Router>
   );
 }
 
